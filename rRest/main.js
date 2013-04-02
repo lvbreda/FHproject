74e0772ce9925a913281d4f0355a6d3baf20b6ab
Lander Van Breda <lvbreda@gmail.com>
2013-04-02 12:06:41 +0200

    Initial setup modules

--- rRest/main.js
+++ rRest/main.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
-
+var restFactory = require("lib/rest.js");
+var socketFactory = require("lib/socket.js");
+var restServers = [];
+var socketServers = [];
 app.configure(function () {
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
@@ -11,9 +14,13 @@ app.configure(function () {
     app.use(express.csrf());
     app.use(express.static(__dirname + '/public'));
 });
-
-
-exports.factory = function (collection, realtime, fields) {
-
+var server = app.listen(3000);
+var io = require('socket.io').listen(server);
+exports.factory = function (db, collection, realtime, options) {
+    restServers.push(restFactory.factory(express, db, collection, options));
+    if (realtime) {
+        socketServers.push(socketFactory.factory(express, io, db, collection, options));
+    }
 }
 exports.app = app;
+
